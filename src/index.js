4971c0581fb298d09a25e953ee663ab62c234347
Justin Lee <justinlee0022@gmail.com>
2017-03-03 17:53:01 -0800

    Adding rependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
+import rootReducer from './reducers'
+import routes from './routes'
+
+import App from './containers/App'
+import './index.css'
+
+
 
 ReactDOM.render(
   <App />,
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,5 @@
+
+
+export default userReducer = (state, action) => {
+    return;
+}
