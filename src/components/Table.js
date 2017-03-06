6a68a5831cc00799c56bfe128a32aeb24f2b6f26
Justin Lee <justinlee0022@gmail.com>
2017-03-05 20:47:06 -0800

    Displayed test data using boostraptable

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,9 +2,16 @@ import React from 'react'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
 
 const Table = (props) => {
+    const data = props.data;
+
+    console.log(data);
     return (
         <div>
-            
+            <BootstrapTable data={data} striped={true} hover={true}>
+                <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Product ID</TableHeaderColumn>
+                <TableHeaderColumn dataField="name" dataSort={true}>Product Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+            </BootstrapTable>
         </div>
     );
 };
--- src/containers/ResearchPage.js
+++ src/containers/ResearchPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Table from '../components/Table'
+import testData from '../constants/testResearchData'
+
 
 class ResearchPage extends Component {
     render() {
         return (
             <div>
-                <Table />            
+                <Table data={testData}/>            
             </div>
         );
     }
